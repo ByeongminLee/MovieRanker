ce7272a167de865caab990e127a30851e8c60487
bmlee531@gmail.com <bmlee531@gmail.com>
2022-02-27 19:35:31 +0900

    Feat: 영화 진흥원 API 연동 slice개발 #2
    - store 등록
    - 영화 진흥원의 영화 순위 API를 호출하는 slice 파일 개발

---
+++ src/slices/MovieRankSlice.js
@@ -0,0 +1,78 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import dayjs from 'dayjs';
+
+export const getRank = createAsyncThunk('/movie/rank', async (payload, { rejectWithValue }) => {
+    if (payload === undefined) {
+        payload = dayjs().add(-1, 'd').format('YYYYMMDD');
+    }
+
+    const targetDt = payload.replaceAll('-', '');
+    let result = null;
+
+    try {
+        const date = dayjs().add(-1, 'd').format('YYYYMMDD');
+        if (parseInt(targetDt) > parseInt(date)) {
+            const err = new Error();
+            err.response = {
+                status: 400,
+                statusText: '조회 가능한 날짜는 하루 전까지만 가능합니다.',
+            };
+            throw err;
+        }
+
+        const apiUrl =
+            'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json';
+
+        result = await axios.get(apiUrl, {
+            params: { key: process.env.REACT_APP_MOVIE_RANK_URL, targetDt: targetDt },
+        });
+
+        if (result.data.faultInfo !== undefined) {
+            const err = new Error();
+            err.response = { status: 500, statusText: result.data.faultInfo.message };
+            throw err;
+        }
+
+        console.log('test1', result);
+    } catch (err) {
+        result = rejectWithValue(err.response);
+    }
+    return result;
+});
+
+const movieRankSlice = createSlice({
+    name: 'movieRank',
+    initialState: {
+        rt: null,
+        rtmsg: null,
+        item: [],
+        loading: false,
+    },
+
+    reducers: {},
+    extraReducers: {
+        [getRank.pending]: (state, { payload }) => {
+            return { ...state, loading: true };
+        },
+        [getRank.fulfilled]: (state, { payload }) => {
+            return {
+                ...state,
+                rt: payload.status,
+                rtmsg: payload.statusText,
+                item: payload.data,
+                loading: false,
+            };
+        },
+        [getRank.rejected]: (state, { payload }) => {
+            return {
+                ...state,
+                rt: payload?.status ? payload.status : '500',
+                rtmsg: payload?.statusText ? payload.statusText : 'Server Error',
+                loading: false,
+            };
+        },
+    },
+});
+
+export default movieRankSlice.reducer;
---
+++ src/store.js
@@ -0,0 +1,14 @@
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { createLogger } from 'redux-logger';
+import movieRankSlice from 'slices/MovieRankSlice';
+
+const logger = createLogger();
+
+const store = configureStore({
+    reducer: {
+        movieRank: movieRankSlice,
+    },
+    middleware: [...getDefaultMiddleware({ serializableCheck: false }), logger],
+    devTools: true,
+});
+export default store;
